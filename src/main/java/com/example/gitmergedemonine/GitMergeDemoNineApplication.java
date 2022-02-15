49b79ded5a84c161bda395b7f95cd4e9008d1a1c
王钰 <wangyu9@bonc.com.cn>
2022-02-15 14:17:14 +0800

    master:第一次提交

--- src/main/java/com/example/gitmergedemonine/GitMergeDemoNineApplication.java
+++ src/main/java/com/example/gitmergedemonine/GitMergeDemoNineApplication.java
@@ -8,6 +8,7 @@ public class GitMergeDemoNineApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GitMergeDemoNineApplication.class, args);
+        System.out.println("123456");
     }
 
 }
