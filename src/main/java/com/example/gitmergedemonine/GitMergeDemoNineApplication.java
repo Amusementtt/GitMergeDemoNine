ae5462610441a814cc9a1ad582d705e352c74b26
王钰 <wangyu9@bonc.com.cn>
2022-02-15 13:01:07 +0800

    提交master分支

---
+++ src/main/java/com/example/gitmergedemonine/GitMergeDemoNineApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitmergedemonine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitMergeDemoNineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitMergeDemoNineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitmergedemonine/GitMergeDemoNineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitmergedemonine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitMergeDemoNineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
